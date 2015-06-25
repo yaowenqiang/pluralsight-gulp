207d8196c26ecd3234755d7e79f8517c86ef680e
yaowenqiang <yaowenqiang111@163.com>
2015-06-26 02:30:38 +0800

    add the styles gulp task

--- gulp.config.js
+++ gulp.config.js
@@ -1,10 +1,17 @@
 module.exports = function () {
+    var client = './src/client/';
     var config = {
         //all the js to wet
+        temp:'./.tmp',
+        /**
+         * files paths
+         */
         alljs:[
         './src/**/*.js',
         './*.js'
-        ]
+        ],
+        //less:'./src/client/styles/styles.less',
+        less:client+'styles/styles.less',
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,14 @@ gulp.task('web',function(){
     .pipe($.jshint.reporter('fail'));
 
 });
+gulp.task('styles',function(){
+    log("Compiling less to css");
+    return gulp
+    .src(config.less)//TODO add the config
+    .pipe($.less())
+    .pipe($.autoprefixer({browsers:['last 2 version','> 5%']}))
+    .pipe(gulp.dest(config.temp))
+});
 ///////
 function log(msg) {
     if (typeof(msg) === 'object') {
