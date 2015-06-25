0721b4eedecd5b4018d2359ca986fdcb346ae284
yaowenqiang <yaowenqiang111@163.com>
2015-06-26 01:48:53 +0800

    add some gulp utilities to print the js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp')
-var jshint = require('gulp-jshint')
-var jscs = require('gulp-jscs')
-var util = require('gulp-util')
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var gulpprint = require('gulp-print');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 //gulp.task('hello-world',function(){
     //console.log('Our hello world gulp task!')
 //});
@@ -12,9 +15,14 @@ gulp.task('web',function(){
         './src/**/*.js',
         './*.js'
     ])
+    //.pipe(gulpprint())
+    //gulp web --verbose
+    .pipe(gulpif(args.verbose,gulpprint()))
     .pipe(jscs())
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish',{verbose:true}));
+    .pipe(jshint.reporter('jshint-stylish',{verbose:true}))
+    .pipe(jshint.reporter('fail'));
+
 });
 ///////
 function log(msg) {
