90093043601fc5bf7cfe9efa8937cbec76d4ef9d
yaowenqiang <yaowenqiang111@163.com>
2015-06-26 01:32:13 +0800

    add log function to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,30 @@
 var gulp = require('gulp')
-gulp.task('hello-world',function(){
-    console.log('Our hello world gulp task!')
+var jshint = require('gulp-jshint')
+var jscs = require('gulp-jscs')
+var util = require('gulp-util')
+//gulp.task('hello-world',function(){
+    //console.log('Our hello world gulp task!')
+//});
+gulp.task('web',function(){
+    log('Analyzing source with JSHint and JSCS.');
+    return gulp
+    .src([
+        './src/**/*.js',
+        './*.js'
+    ])
+    .pipe(jscs())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish',{verbose:true}));
 });
+///////
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg ) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
