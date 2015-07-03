be66420e28d697f67ac7178c7e9cce82beee7c49
jack yao <yaowenqiang111@163.com>
2015-07-04 00:51:24 +0800

    make build env task,the browsersync still has some problem

--- gulpfile.js
+++ gulpfile.js
@@ -81,15 +81,38 @@ gulp.task('wiredep',function(){
 
             .pipe(gulp.dest(config.client));//TODO config
 });
-gulp.task('inject',['wiredep','styles'],function(){
+gulp.task('inject',['wiredep','styles','templatecache'],function(){
     log('Wire up the bower css and js and our app js into the html');
     return gulp
             .src(config.index)//TODO index.html
             .pipe($.inject(gulp.src(config.css)))
             .pipe(gulp.dest(config.client));//TODO config
 });
+
+gulp.task('optimize',['inject'],function(){
+    var assets = $.useref.assets({searchPath:'./'})
+    log('optimizing the javascript,css,html')
+    var templateCache = config.temp + config.templateCache.file;
+    log("the templateCache  is :" + $.util.colors.red(templateCache));
+    return gulp
+            .src(config.index)
+            .pipe($.debug({title:'gulp debug'}))
+            .pipe($.plumber())
+            .pipe($.inject(gulp.src(templateCache,{read:false}),{starttag:'<!-- inject:templates:js -->',endtag:'<!-- endinject -->'}))
+            .pipe(assets)
+            .pipe($.debug({title:'assets debug'}))
+            .pipe(assets.restore())
+            .pipe($.useref())
+            .pipe(gulp.dest(config.build))
+});
+gulp.task('serve-build',['optimize'],function(){
+    serve(false);
+});
 gulp.task('serve-dev',['inject'],function(){
-    var dev = true;
+    serve(true);
+});
+function serve (isDev) {
+    //var dev = isDev;
     var options = {
         script: config.nodeServer, //TODO pp.js
         delayTmme: 1,
@@ -98,7 +121,7 @@ gulp.task('serve-dev',['inject'],function(){
             //"PORT": port? port : 7203,
             //"NODE_ENV":isDev? 'dev' : 'build'
             "PORT": 7203,
-            "NODE_ENV":'dev'
+            "NODE_ENV":isDev ? 'dev' : 'build'
         },
         watch: [config.server]// TODO define the files to restart on
     };
@@ -113,7 +136,7 @@ gulp.task('serve-dev',['inject'],function(){
         })
         .on('start',function(){
             log('*** nodemon started');
-            startBrowserSync();
+            startBrowserSync(isDev);
         })
         .on('crash',function(){
             log('*** nodemon crashed: script crashed for some reason');
@@ -121,7 +144,9 @@ gulp.task('serve-dev',['inject'],function(){
         .on('exit',function(){
             log('*** nodemon exited clearly');
         })
-});
+
+
+}
 gulp.task('help',$.taskListing);
 gulp.task('default',['help']);
 gulp.task('fonts',['clean-fonts'],function(){
@@ -146,25 +171,32 @@ function changeEvent(event) {
     var srcPattern = new RegExp('/.*(?=/' + config.source + ')/')
     log('File ' + event.path.replace(srcPattern,'') + ' ' + event.type);
 }
-function startBrowserSync() {
+function startBrowserSync(isDev) {
     if(args.nosync || browserSync.active) {
         return;
     }
     var port = 7203;
     log("starting browser-sync on port :" + $.util.colors.blue(port));
-    gulp.watch([config.less],['styles'])
-    // TODO change the less files invoke a refresh rather than reload,this is a big issue.
-        .on('change',function(event){
-            changeEvent(event);
+    if(isDev) {
+        gulp.watch([config.less],['styles'])
+        // TODO change the less files invoke a refresh rather than reload,this is a big issue.
+            .on('change',function(event){
+                changeEvent(event);
         });
+    } else {
+        gulp.watch([config.less,config.js,config.html],['optimize',browserSync.reload])
+            .on('change',function(event){
+                changeEvent(event);
+        });
+    }
     var options = {
         proxy: "localhost:" + port,
         port: 3000,
-        files: [
+        files: isDev? [
             //config.client + '**/*.*',
             //'!' + config.less,
             config.temp + '**/*.css'
-        ],
+        ]:[],
         ghostMode:{
             clicks:true,
             location:true,
